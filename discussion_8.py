453044ae9c820c51a68855328a50bbee0049c822
Clare McNamara <clarem@Clares-Air-2.attlocal.net>
2022-10-25 21:27:56 -0400

    first commit

--- discussion_8.py
+++ discussion_8.py
@@ -1,26 +1,42 @@
+#Name: Clare McNamara 
+#UMID: 07387913
+
 from bs4 import BeautifulSoup
 import requests
 import unittest
+
 
 # Task 2: Look at the Get the URL that links to webpage of universities with Olympic medal wins
 # search for the url in the University of Michgian wikipedia page (in the third pargraph of the intro)
 # HINT: You will have to add https://en.wikipedia.org to the URL retrieved using BeautifulSoup
 def getLink(soup):
-    
-    pass
+    url_base = "https://en.wikipedia.org"
+    tag = soup.find('a', class_='mw-redirect')
+    info = tag.get('href')
+    return url_base + info
 
 # Task 3: Get the details from the box titled "College/school founding". Get all the college/school names and the year they were
 # founded and organize the same into key-value pairs.
 def getAdmissionsInfo2019(soup):
+    school_founding = {}
+    big_box = soup.find('table' , class_="toccolours")
+    lines = big_box.findall("tr")
+    for line in lines:
+        both = line.findall('td')
+        school_name = both[0]
+        year_founded = both[1]
+        school_founding[school_name] = year_founded
+    return school_founding
 
-    pass
 
 
 
 def main():
     # Task 1: Create a BeautifulSoup object and name it soup. Refer to discussion slides or lecture slides to complete this
 
-    #### YOUR CODE HERE####
+    url = "https://en.wikipedia.org/wiki/University_of_Michigan"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
 
     #Call the functions getLink(soup) and getAdmissionsInfo2019(soup) on your soup object.
     getLink(soup)
